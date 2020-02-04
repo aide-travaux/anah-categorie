32831511ac1c56fd6e9a8ed16aff9aaaf53c8bf8
Adrien Rosi <adrienrosi@gmail.com>
2020-02-04 15:03:54 +0100

    Version 1.0.0

--- AnahCategorie.php
+++ AnahCategorie.php
@@ -2,18 +2,8 @@
 
 namespace AideTravaux\Anah\Categorie;
 
-use AideTravaux\Core\Data\Entries;
-
 class AnahCategorie
 {
-    /**
-     * Liste des codes région
-     */
-    const CODES_REGION = [
-        "01", "02", "03", "04", "06", "11", "24", "27", "28",
-        "32", "44", "52", "53", "75", "76", "84", "93", "94"
-    ];
-
     /**
      * 
      * @param int Ressources du foyer
@@ -21,117 +11,105 @@ class AnahCategorie
      * @param string Code administratif de la région
      * @return null|string
      */
-    public static function get(int $income, int $household, string $codeRegion): ?string
+    public static function get(int $ressources, int $compositionFoyer, string $codeRegion): ?string
     {
-        self::validateHousehold($household);
-        self::validateCode($codeRegion);
+        if (!in_array($codeRegion, Entries::CODES_REGION) || !$compositionFoyer) {
+            return null;
+        }
 
         switch ($codeRegion) {
             case '11':
-                switch ($household) {
+                switch ($compositionFoyer) {
                     case 1:
-                        if ($income < 20593) {
-                            return Entries::ANAH_CATEGORIES['anah_category_2'];
-                        } else if ($income < 25068) {
-                            return Entries::ANAH_CATEGORIES['anah_category_1'];
+                        if ($ressources < 20593) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_2'];
+                        } else if ($ressources < 25068) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_1'];
                         }
                         break;
                     case 2:
-                        if ($income < 30225) {
-                            return Entries::ANAH_CATEGORIES['anah_category_2'];
-                        } else if ($income < 36792) {
-                            return Entries::ANAH_CATEGORIES['anah_category_1'];
+                        if ($ressources < 30225) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_2'];
+                        } else if ($ressources < 36792) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_1'];
                         }
                         break;
                     case 3:
-                        if ($income < 36297) {
-                            return Entries::ANAH_CATEGORIES['anah_category_2'];
-                        } else if ($income < 44188) {
-                            return Entries::ANAH_CATEGORIES['anah_category_1'];
+                        if ($ressources < 36297) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_2'];
+                        } else if ($ressources < 44188) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_1'];
                         }
                         break;
                     case 4:
-                        if ($income < 42381) {
-                            return Entries::ANAH_CATEGORIES['anah_category_2'];
-                        } else if ($income < 51597) {
-                            return Entries::ANAH_CATEGORIES['anah_category_1'];
+                        if ($ressources < 42381) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_2'];
+                        } else if ($ressources < 51597) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_1'];
                         }
                         break;
                     case 5:
-                        if ($income < 48488) {
-                            return Entries::ANAH_CATEGORIES['anah_category_2'];
-                        } else if ($income < 59026) {
-                            return Entries::ANAH_CATEGORIES['anah_category_1'];
+                        if ($ressources < 48488) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_2'];
+                        } else if ($ressources < 59026) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_1'];
                         }
                         break;
                     default:
-                        if ($income < 48488 + ($household - 5) * 6096) {
-                            return Entries::ANAH_CATEGORIES['anah_category_2'];
-                        } else if ($income < 59026 + ($household - 5) * 7422) {
-                            return Entries::ANAH_CATEGORIES['anah_category_1'];
+                        if ($ressources < 48488 + ($compositionFoyer - 5) * 6096) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_2'];
+                        } else if ($ressources < 59026 + ($compositionFoyer - 5) * 7422) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_1'];
                         }
                 }
                 return null;
             
             default:
-                switch ($household) {
+                switch ($compositionFoyer) {
                     case 1:
-                        if ($income < 14879) {
-                            return Entries::ANAH_CATEGORIES['anah_category_2'];
-                        } else if ($income < 19074) {
-                            return Entries::ANAH_CATEGORIES['anah_category_1'];
+                        if ($ressources < 14879) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_2'];
+                        } else if ($ressources < 19074) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_1'];
                         }
                         break;
                     case 2:
-                        if ($income < 21760) {
-                            return Entries::ANAH_CATEGORIES['anah_category_2'];
-                        } else if ($income < 27896) {
-                            return Entries::ANAH_CATEGORIES['anah_category_1'];
+                        if ($ressources < 21760) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_2'];
+                        } else if ($ressources < 27896) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_1'];
                         }
                         break;
                     case 3:
-                        if ($income < 26170) {
-                            return Entries::ANAH_CATEGORIES['anah_category_2'];
-                        } else if ($income < 33547) {
-                            return Entries::ANAH_CATEGORIES['anah_category_1'];
+                        if ($ressources < 26170) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_2'];
+                        } else if ($ressources < 33547) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_1'];
                         }
                         break;
                     case 4:
-                        if ($income < 30572) {
-                            return Entries::ANAH_CATEGORIES['anah_category_2'];
-                        } else if ($income < 39192) {
-                            return Entries::ANAH_CATEGORIES['anah_category_1'];
+                        if ($ressources < 30572) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_2'];
+                        } else if ($ressources < 39192) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_1'];
                         }
                         break;
                     case 5:
-                        if ($income < 34993) {
-                            return Entries::ANAH_CATEGORIES['anah_category_2'];
-                        } else if ($income < 44860) {
-                            return Entries::ANAH_CATEGORIES['anah_category_1'];
+                        if ($ressources < 34993) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_2'];
+                        } else if ($ressources < 44860) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_1'];
                         }
                         break;
                     default:
-                        if ($income < 34993 + ($household - 5) * 4412) {
-                            return Entries::ANAH_CATEGORIES['anah_category_2'];
-                        } else if ($income < 44860 + ($household - 5) * 5651) {
-                            return Entries::ANAH_CATEGORIES['anah_category_1'];
+                        if ($ressources < 34993 + ($compositionFoyer - 5) * 4412) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_2'];
+                        } else if ($ressources < 44860 + ($compositionFoyer - 5) * 5651) {
+                            return Entries::CATEGORIES_ANAH['cateogrie_anah_1'];
                         }
                 }
                 return null;
         }
     }
 
-    private static function validateCode(string $codeRegion)
-    {
-        if (!\in_array($codeRegion, self::CODES_REGION)) {
-            throw new \Exception("Invalid code region value");
-        }
-    }
-
-    private static function validateHousehold(int $household)
-    {
-        if (!$household) {
-            throw new \Exception("Invalid household value");
-        }
-    }
 }
--- Tests/AnahCategorieTest.php
+++ Tests/AnahCategorieTest.php
@@ -4,28 +4,16 @@ namespace AideTravaux\Anah\Categorie\Tests;
 
 use PHPUnit\Framework\TestCase;
 use AideTravaux\Anah\Categorie\AnahCategorie;
-use AideTravaux\Core\Data\Entries;
+use AideTravaux\Anah\Categorie\Entries;
 
 class AnahCategorieTest extends TestCase
 {
-    public function testInvalidHousehold()
-    {
-        $this->expectException(\Exception::class);
-        AnahCategorie::get(1, 0, '01');
-    }
-
-    public function testInvalidCodeRegion()
-    {
-        $this->expectException(\Exception::class);
-        AnahCategorie::get(1, 1, 'CODE');
-    }
-
     /**
      * @dataProvider simulationProvider
      */
-    public function testSimulation(int $income, int $household, string $codeRegion, ?string $expect)
+    public function testSimulation(int $ressources, int $compositionFoyer, string $codeRegion, ?string $expect)
     {
-        $this->assertEquals(AnahCategorie::get($income, $household, $codeRegion), $expect);
+        $this->assertEquals(AnahCategorie::get($ressources, $compositionFoyer, $codeRegion), $expect);
     }
 
     public function simulationProvider()
@@ -38,19 +26,19 @@ class AnahCategorieTest extends TestCase
             [59026, 5, '11', null],
             [59026 + 7422, 6, '11', null],
 
-            [25068 - 1, 1, '11', Entries::ANAH_CATEGORIES['anah_category_1']],
-            [36792 - 1, 2, '11', Entries::ANAH_CATEGORIES['anah_category_1']],
-            [44188 - 1, 3, '11', Entries::ANAH_CATEGORIES['anah_category_1']],
-            [51597 - 1, 4, '11', Entries::ANAH_CATEGORIES['anah_category_1']],
-            [59026 - 1, 5, '11', Entries::ANAH_CATEGORIES['anah_category_1']],
-            [59026 + 7422 - 1, 6, '11', Entries::ANAH_CATEGORIES['anah_category_1']],
+            [25068 - 1, 1, '11', Entries::CATEGORIES_ANAH['cateogrie_anah_1']],
+            [36792 - 1, 2, '11', Entries::CATEGORIES_ANAH['cateogrie_anah_1']],
+            [44188 - 1, 3, '11', Entries::CATEGORIES_ANAH['cateogrie_anah_1']],
+            [51597 - 1, 4, '11', Entries::CATEGORIES_ANAH['cateogrie_anah_1']],
+            [59026 - 1, 5, '11', Entries::CATEGORIES_ANAH['cateogrie_anah_1']],
+            [59026 + 7422 - 1, 6, '11', Entries::CATEGORIES_ANAH['cateogrie_anah_1']],
 
-            [20593 - 1, 1, '11', Entries::ANAH_CATEGORIES['anah_category_2']],
-            [30225 - 1, 2, '11', Entries::ANAH_CATEGORIES['anah_category_2']],
-            [36297 - 1, 3, '11', Entries::ANAH_CATEGORIES['anah_category_2']],
-            [42381 - 1, 4, '11', Entries::ANAH_CATEGORIES['anah_category_2']],
-            [6096 - 1, 5, '11', Entries::ANAH_CATEGORIES['anah_category_2']],
-            [6096 + 6096 - 1, 6, '11', Entries::ANAH_CATEGORIES['anah_category_2']],
+            [20593 - 1, 1, '11', Entries::CATEGORIES_ANAH['cateogrie_anah_2']],
+            [30225 - 1, 2, '11', Entries::CATEGORIES_ANAH['cateogrie_anah_2']],
+            [36297 - 1, 3, '11', Entries::CATEGORIES_ANAH['cateogrie_anah_2']],
+            [42381 - 1, 4, '11', Entries::CATEGORIES_ANAH['cateogrie_anah_2']],
+            [6096 - 1, 5, '11', Entries::CATEGORIES_ANAH['cateogrie_anah_2']],
+            [6096 + 6096 - 1, 6, '11', Entries::CATEGORIES_ANAH['cateogrie_anah_2']],
 
             [19074, 1, '02', null],
             [27896, 2, '02', null],
@@ -59,19 +47,19 @@ class AnahCategorieTest extends TestCase
             [44860, 5, '02', null],
             [44860 + 5651, 6, '02', null],
 
-            [19074 - 1, 1, '02', Entries::ANAH_CATEGORIES['anah_category_1']],
-            [27896 - 1, 2, '02', Entries::ANAH_CATEGORIES['anah_category_1']],
-            [33547 - 1, 3, '02', Entries::ANAH_CATEGORIES['anah_category_1']],
-            [39192 - 1, 4, '02', Entries::ANAH_CATEGORIES['anah_category_1']],
-            [44860 - 1, 5, '02', Entries::ANAH_CATEGORIES['anah_category_1']],
-            [44860 + 5651 - 1, 6, '02', Entries::ANAH_CATEGORIES['anah_category_1']],
+            [19074 - 1, 1, '02', Entries::CATEGORIES_ANAH['cateogrie_anah_1']],
+            [27896 - 1, 2, '02', Entries::CATEGORIES_ANAH['cateogrie_anah_1']],
+            [33547 - 1, 3, '02', Entries::CATEGORIES_ANAH['cateogrie_anah_1']],
+            [39192 - 1, 4, '02', Entries::CATEGORIES_ANAH['cateogrie_anah_1']],
+            [44860 - 1, 5, '02', Entries::CATEGORIES_ANAH['cateogrie_anah_1']],
+            [44860 + 5651 - 1, 6, '02', Entries::CATEGORIES_ANAH['cateogrie_anah_1']],
 
-            [14879 - 1, 1, '02', Entries::ANAH_CATEGORIES['anah_category_2']],
-            [21760 - 1, 2, '02', Entries::ANAH_CATEGORIES['anah_category_2']],
-            [26170 - 1, 3, '02', Entries::ANAH_CATEGORIES['anah_category_2']],
-            [30572 - 1, 4, '02', Entries::ANAH_CATEGORIES['anah_category_2']],
-            [34993 - 1, 5, '02', Entries::ANAH_CATEGORIES['anah_category_2']],
-            [34993 + 4412 - 1, 6, '02', Entries::ANAH_CATEGORIES['anah_category_2']]
+            [14879 - 1, 1, '02', Entries::CATEGORIES_ANAH['cateogrie_anah_2']],
+            [21760 - 1, 2, '02', Entries::CATEGORIES_ANAH['cateogrie_anah_2']],
+            [26170 - 1, 3, '02', Entries::CATEGORIES_ANAH['cateogrie_anah_2']],
+            [30572 - 1, 4, '02', Entries::CATEGORIES_ANAH['cateogrie_anah_2']],
+            [34993 - 1, 5, '02', Entries::CATEGORIES_ANAH['cateogrie_anah_2']],
+            [34993 + 4412 - 1, 6, '02', Entries::CATEGORIES_ANAH['cateogrie_anah_2']]
         ];
     }
 
